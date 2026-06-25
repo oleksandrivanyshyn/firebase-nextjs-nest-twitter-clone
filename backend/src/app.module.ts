abbbbad4ca127e6df63d3aedc71c4aeb0022b289
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-25 18:53:20 +0300

    style: enforce consistent code style with Prettier integration and formatting adjustments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FirebaseModule } from './integrations/firebase/firebase.module';
-import {ConfigModule} from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [FirebaseModule, ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-  })],
+  imports: [
+    FirebaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
