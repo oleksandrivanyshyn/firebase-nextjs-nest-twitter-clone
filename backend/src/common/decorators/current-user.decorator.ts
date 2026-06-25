1768c734687393e450ce8378375ad5ac6c0cd382
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-25 18:09:03 +0300

    feat(back): add AuthGuard and CurrentUser decorator for Firebase authentication

---
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import type { DecodedIdToken } from 'firebase-admin/auth';
+
+export const CurrentUser = createParamDecorator(
+    (_data: unknown, ctx: ExecutionContext): DecodedIdToken => {
+        return ctx.switchToHttp().getRequest().user;
+    },
+);
