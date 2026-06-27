ab06084302e1f99969069df9e7af955432b59ff8
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-27 22:57:54 +0300

    feat(back): register posts, reactions, and comments modules in app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
-import { FirebaseModule } from './integrations/firebase/firebase.module';
 import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './integrations/firebase/firebase.module';
 import { UserModule } from './modules/user/user.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { ReactionsModule } from './modules/reactions/reactions.module';
+import { CommentsModule } from './modules/comments/comments.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     FirebaseModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
     UserModule,
+    PostsModule,
+    ReactionsModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
